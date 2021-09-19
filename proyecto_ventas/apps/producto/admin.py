b254db63fba4135a2f040e7d57096ebcc2b14e35
Piero <pieroe.moscoso@gmail.com>
2021-09-18 19:10:46 -0500

    app Producto con modelos asignados en Miro

--- proyecto_ventas/apps/producto/admin.py
+++ proyecto_ventas/apps/producto/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.producto.models import categoria,proveedor,producto,compra
 
 # Register your models here.
+admin.site.register(categoria)
+admin.site.register(proveedor)
+admin.site.register(producto)
+admin.site.register(compra)
--- proyecto_ventas/apps/producto/models.py
+++ proyecto_ventas/apps/producto/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class categoria(models.Model):
+    """Model definition for categoria."""
+
+    nombre = models.CharField(null=False, blank=False, max_length=50)
+
+    class Meta:
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categorias'
+
+    def __str__(self):
+        return self.nombre
+
+class producto(models.Model):
+
+    nombre = models.CharField(null=False, blank=False, max_length=50)
+    marca = models.CharField(null=False, blank=False, max_length=50)
+    costo = models.FloatField()
+    precio_venta = models.FloatField()
+    id_categiria = models.ForeignKey(categoria, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'producto'
+        verbose_name_plural = 'productos'
+
+    def __str__(self):
+        return self.nombre
+
+class proveedor(models.Model):
+
+    nombre = models.CharField(null=False, blank=False, max_length=50)
+    telefono = models.IntegerField()
+    ruc = models.IntegerField()
+    direccion = models.CharField(null=False, blank=False, max_length=50)
+    id_producto = models.ForeignKey(producto,on_delete=CASCADE)
+
+    class Meta:
+        
+        verbose_name = 'proveedor'
+        verbose_name_plural = 'proveedores'
+
+    def __str__(self):
+        return self.nombre
+
+class compra(models.Model):
+
+    cantidad = models.IntegerField()
+    id_producto = models.ForeignKey(producto, on_delete=models.CASCADE)
+    id_proveedor = models.ForeignKey(proveedor, on_delete=models.CASCADE)
+    fecha = models.DateField(auto_now=False, auto_now_add=False)
+
+    class Meta:
+        verbose_name = 'compra'
+        verbose_name_plural = 'compras'
+
+    def __str__(self):
+        return self.fecha
